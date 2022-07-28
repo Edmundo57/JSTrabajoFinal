d52eb0683d00ba9d25f764d3d7fcd03405890ef1
unknown <sistemas@laptsistemas.valhpac.com>
2022-07-28 17:09:03 -0600

    V2.1

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 let cuentas = [
-    { id: 1, nombre: 'Jesus', saldo: 100, password: '1234' }, { id: 2, nombre: 'Edmundo', saldo: 20, password: '7777' }, { id: 3, nombre: 'Consuelo', saldo: 50, password: '9999' }
+    { id: 1, nombre: 'Jesus', saldo: 100, password: '1234' }, { id: 2, nombre: 'Edmundo', saldo: 20, password: '8888' }, { id: 3, nombre: 'Consuelo', saldo: 50, password: '9999' }
 ];
 
 export { cuentas };
